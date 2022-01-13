9b02440f7290b34f7bd9f9afbbe30e562c11fb5c
Michał Bidziński <michal.bidzinski1337@gmail.com>
2022-01-13 21:53:58 +0100

    zad2

---
+++ zad2/src/Roman.py
@@ -0,0 +1,11 @@
+def roman(num):
+    arabic = [1000, 900, 500, 400, 100, 90, 50,  40, 10, 9, 5, 4, 1]
+    syb = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    roman = ''
+    i = 0
+    while num > 0:
+        for _ in range(num // arabic[i]):
+            roman += syb[i]
+            num -= arabic[i]
+        i += 1
+    return roman
